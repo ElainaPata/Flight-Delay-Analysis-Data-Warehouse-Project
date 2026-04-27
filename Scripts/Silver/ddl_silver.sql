694652cb5c71f67c52e77327e45d7a64e326a1ed
Elaina Pata <80792074+ElainaPata@users.noreply.github.com>
2026-04-27 14:44:23 -0500

    Create ddl_silver.sql

---
+++ Scripts/Silver/ddl_silver.sql
@@ -0,0 +1,9 @@
+/*
+-----------------------------------------------------
+DDL script: Create Silver Tables
+-----------------------------------------------------
+Script Purpose: 
+  This script creates tables in the 'silver' schema, dropping exsisting tables if they already exsist.
+  Run this script to redefine the DDL structure of the silver table.
+------------------------------------------------------------------------------------------------------
+*/
