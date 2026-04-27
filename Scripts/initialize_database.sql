a8d616a3d0529a03a3fe2bbb5dd0edd6bda0b50b
Elaina Pata <80792074+ElainaPata@users.noreply.github.com>
2026-04-27 12:16:53 -0500

    Update initialize_database.sql

--- Scripts/initialize_database.sql
+++ Scripts/initialize_database.sql
@@ -1,1 +1,21 @@
+/* 
+=====================================
+Create Database and Schemas
+=====================================
+Script Purpose: 
+This script creates a new database named 'DataWarehouse'. The script also sets up three schemas within the database: 
+'bronze', 'silver', and 'gold.
+*/
 
+
+Use master
+
+--Create the database 'DataWarehouse'
+Create DATABASE DataWarehouse
+
+Use DataWarehouse
+
+--Creating Schema Layers
+Create SCHEMA bronze
+Create SCHEMA silver 
+Create SCHEMA gold
