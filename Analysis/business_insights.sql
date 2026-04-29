97e56d115b4b98f1b7258c4ccc10f6b4dbc6a51f
Elaina Pata <80792074+ElainaPata@users.noreply.github.com>
2026-04-29 09:23:36 -0500

    Create business_insights.sql

---
+++ Analysis/business_insights.sql
@@ -0,0 +1,13 @@
+/*
+===========================================================
+Flight Delay Analysis – Business Insights
+===========================================================
+Purpose:
+    This script answers key business questions using the
+    Gold layer of the data warehouse.
+===========================================================
+*/
+
+-- =========================================================
+-- 1. Which airlines have the highest average arrival delay?
+-- =========================================================
